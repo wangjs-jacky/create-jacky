8c282039b07bfabce51790bb641fb3a6dfed14d4
所无聊 <2409277719@qq.com>
2023-06-14 01:07:53 +0800

    💥 feat: 集成 ora 依赖

--- generators/test/index.js
+++ generators/test/index.js
@@ -1,53 +1,65 @@
 const BasicGenerator = require('../../dist/BasicGenerator.js');
+const ora = require("ora");
 
 class Generator extends BasicGenerator {
   /* 指定列表 */
-  prompting() {
-    const prompts = [
-      {
-        name: 'name',
-        message: `What's the plugin name?`,
-        default: this.name,
-      },
-      {
-        name: 'description',
-        message: `What's your plugin used for?`,
-      },
-      {
-        name: 'mail',
-        message: `What's your email?`,
-      },
-      {
-        name: 'author',
-        message: `What's your name?`,
-      },
-      {
-        name: 'org',
-        message: `Which organization is your plugin stored under github?`,
-      },
-      {
-        name: 'isTypeScript',
-        type: 'list',
-        message: 'Select the development language',
-        choices: [
+  async prompting() {
+    const spinner = ora('查询仓库中')
+    const getPrompts = () => new Promise((resolve) => {
+      spinner.start();
+
+      setTimeout(() => {
+        resolve([
           {
-            name: 'TypeScript',
-            value: true,
+            name: 'name',
+            message: `What's the plugin name?`,
+            default: this.name,
           },
           {
-            name: 'JavaScript',
-            value: false,
+            name: 'description',
+            message: `What's your plugin used for?`,
           },
-        ],
-        default: true,
-      },
-      {
-        name: 'withUmiUI',
-        type: 'confirm',
-        message: 'Does your plugin have ui interaction(umi ui)?',
-        default: false,
-      },
-    ];
+          {
+            name: 'mail',
+            message: `What's your email?`,
+          },
+          {
+            name: 'author',
+            message: `What's your name?`,
+          },
+          {
+            name: 'org',
+            message: `Which organization is your plugin stored under github?`,
+          },
+          {
+            name: 'isTypeScript',
+            type: 'list',
+            message: 'Select the development language',
+            choices: [
+              {
+                name: 'TypeScript',
+                value: true,
+              },
+              {
+                name: 'JavaScript',
+                value: false,
+              },
+            ],
+            default: true,
+          },
+          {
+            name: 'withUmiUI',
+            type: 'confirm',
+            message: 'Does your plugin have ui interaction(umi ui)?',
+            default: false,
+          },
+        ])
+      }, 3000)
+    });
+
+    const prompts = await getPrompts();
+    spinner.stop();
+
     return this.prompt(prompts).then(props => {
       this.prompts = props;
     });
